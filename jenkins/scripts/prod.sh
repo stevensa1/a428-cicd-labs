1f5381db2fc0770817eb2c0970aab70db51b97b8
Dicoding Indonesia <steven.apps.paid@gmail.com>
2023-11-13 16:08:16 +0700

    Fixed hanging scripts when running the production build

--- jenkins/scripts/prod.sh
+++ jenkins/scripts/prod.sh
@@ -14,9 +14,9 @@ set +x
 echo 'Building your Node.js/React application for production...'
 set -x
 npm run build
-sudo serve -s build -l 80 &
-sleep 1
-echo $! > .pidfile
+nohup sudo serve -s build -l 80 > serve.log 2>&1 &
+SERVER_PID=$!
+echo $SERVER_PID > .pidfile
 set +x
 
 echo 'Build complete.'
